ab140a558830c9b8d5d4d9af3db6a81dbbb5e4b3
EC2 Default User <ec2-user@ip-172-16-69-89.ap-northeast-1.compute.internal>
2019-06-04 09:53:33 +0000

    add data prepare

--- run_classifier.sh
+++ run_classifier.sh
@@ -1,13 +1,13 @@
 #!/usr/bin/env bash
 
 export BERT_BASE_DIR=/home/ec2-user/SageMaker/bert/bert_model/chinese_L-12_H-768_A-12
-export GLUE_DIR=/home/ubuntu/glue_data
+export GLUE_DIR=/home/ec2-user/SageMaker/bert/data
 
 python run_classifier.py \
-  --task_name=MRPC \
+  --task_name=mytask \
   --do_train=true \
   --do_eval=true \
-  --data_dir=$GLUE_DIR/MRPC \
+  --data_dir=$GLUE_DIR/mytask \
   --vocab_file=$BERT_BASE_DIR/vocab.txt \
   --bert_config_file=$BERT_BASE_DIR/bert_config.json \
   --init_checkpoint=$BERT_BASE_DIR/bert_model.ckpt \
@@ -15,5 +15,5 @@ python run_classifier.py \
   --train_batch_size=32 \
   --learning_rate=2e-5 \
   --num_train_epochs=3.0 \
-  --output_dir=/tmp/mrpc_output/ \
+  --output_dir=/tmp/mytask_output/ \
   --use_tpu=False
