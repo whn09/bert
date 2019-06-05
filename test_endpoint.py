330bde0d6fbf2e48c2ad3380c441e3626304f2ce
EC2 Default User <ec2-user@ip-172-16-93-253.ap-northeast-1.compute.internal>
2019-06-05 08:32:32 +0000

    add new text

--- test_endpoint.py
+++ test_endpoint.py
@@ -5,7 +5,9 @@ client = boto3.client('sagemaker-runtime')
 endpoint_name = "mia-mytask-endpoint"                                       # Your endpoint name.
 content_type = "application/jsonlines"                                        # The MIME type of the input data in the request body.
 accept = "text/json"                                              # The desired MIME type of the inference in the response.
-payload = '{"source":"帮 我 尽快 发货 ， 家里 没有 了"}'                                         # Payload for inference.
+#payload = '{"source":"帮 我 尽快 发货 ， 家里 没有 了"}'                                         # Payload for inference.
+payload = '{"source":"能不能 快点 发货"}'                                         # Payload for inference.
+
  
 response = client.invoke_endpoint(
     EndpointName=endpoint_name, 
