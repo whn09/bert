0795ecc34a5130f993b62776a2897572531a960e
EC2 Default User <ec2-user@ip-172-16-93-253.ap-northeast-1.compute.internal>
2019-06-05 08:08:26 +0000

    add test_endpoint

--- test_endpoint.py
+++ test_endpoint.py
@@ -3,7 +3,7 @@ import boto3
 client = boto3.client('sagemaker-runtime')
 
 endpoint_name = "mia-mytask-endpoint"                                       # Your endpoint name.
-content_type = "text/csv;label_size=0"                                        # The MIME type of the input data in the request body.
+content_type = "text/plain"                                        # The MIME type of the input data in the request body.
 accept = "text/json"                                              # The desired MIME type of the inference in the response.
 payload = "帮 我 尽快 发货 ， 家里 没有 了"                                             # Payload for inference.
  
