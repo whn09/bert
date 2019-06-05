20198ca33676e04f9bca1694b998cb43829cf447
EC2 Default User <ec2-user@ip-172-16-93-253.ap-northeast-1.compute.internal>
2019-06-05 08:27:37 +0000

    fix test_endpint bug

--- test_endpoint.py
+++ test_endpoint.py
@@ -3,9 +3,9 @@ import boto3
 client = boto3.client('sagemaker-runtime')
 
 endpoint_name = "mia-mytask-endpoint"                                       # Your endpoint name.
-content_type = "text/plain"                                        # The MIME type of the input data in the request body.
+content_type = "application/jsonlines"                                        # The MIME type of the input data in the request body.
 accept = "text/json"                                              # The desired MIME type of the inference in the response.
-payload = "帮 我 尽快 发货 ， 家里 没有 了"                                             # Payload for inference.
+payload = '{"source":"帮 我 尽快 发货 ， 家里 没有 了"}'                                         # Payload for inference.
  
 response = client.invoke_endpoint(
     EndpointName=endpoint_name, 
